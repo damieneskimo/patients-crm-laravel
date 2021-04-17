68b4a3c759f79afbb448ac22e44dba49e221b358
Damien Jiang <damieneskimo@gmail.com>
2021-04-17 16:24:22 +1200

    add one more normal use in db seed for testing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,8 +24,17 @@ class DatabaseSeeder extends Seeder
             'password' => bcrypt('password'),
             'remember_token' => Str::random(10),
         ]);
+        \App\Models\User::create([
+            'name' => 'normal user',
+            'email' => 'test@gmail.com',
+            'mobile' => '123456',
+            'gender' => 'male',
+            'email_verified_at' => now(),
+            'is_admin' => 0,
+            'password' => bcrypt('password'),
+        ]);
 
-        \App\Models\User::factory(50)->hasNotes(3)->create();
+        \App\Models\User::factory(50)->hasNotes(5)->create();
 
     }
 }
