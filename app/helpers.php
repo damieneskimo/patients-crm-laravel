0a74adff00aa89fdd85511adb74ee4479729856c
Damien Jiang <damieneskimo@gmail.com>
2021-08-31 11:08:38 +1200

    add helper method

--- app/helpers.php
+++ app/helpers.php
@@ -3,3 +3,9 @@
 function resource_data($resource) {
     return $resource->response()->getData(true);
 }
+
+function get_model_name_from_controller($controller) {
+    $arr = preg_split('/(?=[A-Z])/', $controller, -1, PREG_SPLIT_NO_EMPTY);
+
+    return ! empty($arr)? $arr[0] : '';
+}
